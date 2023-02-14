5f8832df5b317b272da3ef94791275fa21a54378
Mariya Malakhova <malakhova@siberian.pro>
2023-02-14 16:55:32 +0700

    fix: верстка страницы с поиском упражнение

--- src/main/kotlin/nsu/fit/qyoga/core/exercises/api/model/Exercise.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/exercises/api/model/Exercise.kt
@@ -1,4 +1,17 @@
 package nsu.fit.qyoga.core.exercises.api.model
 
-class Exercise {
-}
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("exercises")
+data class Exercise(
+    @Id
+    val id: Long,
+    val title: String,
+    val description: String,
+    val indications: String,
+    val contradictions: String,
+    val duration: String,
+    val exerciseType: ExerciseType,
+    val therapistId: Long
+)
