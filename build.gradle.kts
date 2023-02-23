81c3bf10ada1fa94ae84e5a75eb1966886506e87
Aleksey <me@azhidkov.pro>
2023-02-23 07:19:02 +0700

    build: проект переведён на Boot 3 и Kotlin 1.8 (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.4"
-	id("io.spring.dependency-management") version "1.0.14.RELEASE"
-	kotlin("jvm") version "1.7.20"
-	kotlin("plugin.spring") version "1.7.20"
+	id("org.springframework.boot") version "3.0.2"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.8.10"
+	kotlin("plugin.spring") version "1.8.10"
 	id("io.gitlab.arturbosch.detekt") version "1.22.0"
 	id("org.jetbrains.kotlinx.kover") version "0.6.1"
 }
