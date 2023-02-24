9ab6d430cadbe616a58b3a3bd351f0f6719c8a53
Mariya Malakhova <malakhova@siberian.pro>
2023-02-25 04:40:03 +0700

    feat: добавлен сабмит формы для одновременного запроса всех параметров

--- src/main/kotlin/nsu/fit/qyoga/core/exercises/api/dtos/ExerciseSearchDto.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/exercises/api/dtos/ExerciseSearchDto.kt
@@ -3,7 +3,7 @@ package nsu.fit.qyoga.core.exercises.api.dtos
 import nsu.fit.qyoga.core.exercises.api.model.ExerciseType
 
 data class ExerciseSearchDto(
-    val title: String? = "",
+    val title: String? = null,
     val contradiction: String? = null,
     val duration: String? = null,
     val exerciseType: ExerciseType? = null,
