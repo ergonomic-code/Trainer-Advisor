7b0b9a9e99eb9ae9133052d2732d68b20852cc2a
Mariya Malakhova <malakhova@siberian.pro>
2023-02-25 03:03:10 +0700

    feat: фильтрация

---
+++ src/main/kotlin/nsu/fit/qyoga/core/exercises/api/dtos/ExerciseSearchDto.kt
@@ -0,0 +1,11 @@
+package nsu.fit.qyoga.core.exercises.api.dtos
+
+import nsu.fit.qyoga.core.exercises.api.model.ExerciseType
+
+data class ExerciseSearchDto(
+    val title: String? = "",
+    val contradiction: String? = null,
+    val duration: String? = null,
+    val exerciseType: ExerciseType? = null,
+    val therapeuticPurpose: String? = null,
+)
