4d4b112bcde643130895e06aec9888d41d6e5f77
Mariya Malakhova <malakhova@siberian.pro>
2023-03-03 11:13:56 +0700

    fix: тест с фильтров на типы упражнений

--- src/main/kotlin/nsu/fit/qyoga/core/exercises/internal/ExercisesRepo.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/exercises/internal/ExercisesRepo.kt
@@ -22,11 +22,11 @@ interface ExercisesRepo : CrudRepository<Exercise, Long>, PagingAndSortingReposi
             INNER JOIN exercise_purposes ep ON ex.id = ep.exercise_id
             INNER JOIN therapeutic_purposes tp ON tp.id = ep.purpose_id
             INNER JOIN exercise_types et on ex.exercise_type_id = et.id
-        WHERE (:#{#search?.title ?: ""} IS NULL OR ex.title LIKE '%' || :#{#search?.title ?: ""} || '%')
-            AND (ex.contradictions LIKE '%' || :#{#search?.contradiction ?: ""} || '%' OR :#{#search?.contradiction ?: ""} IS NULL)
-            AND (:#{#search.duration}::interval IS NULL OR ex.duration = :#{#search.duration}::interval)
-            AND (et.name LIKE '%' || :#{#search?.exerciseType ?: ""} || '%' OR :#{#search?.exerciseType ?: ""} IS NULL)
-            AND (tp.purpose LIKE '%' || :#{#search?.therapeuticPurpose ?: ""} || '%' OR :#{#search?.therapeuticPurpose ?: ""} IS NULL)
+        WHERE (ex.title LIKE '%' || :#{#search.title ?: ""} || '%' OR :#{#search.title ?: ""} IS NULL)
+            AND (ex.contradictions LIKE '%' || :#{#search?.contradiction ?: ""} || '%' OR :#{#search.contradiction ?: ""} IS NULL)
+            AND (:#{#search.duration} IS NULL OR ex.duration = :#{#search.duration}::interval)
+            AND (et.name LIKE '%' || :#{#search.exerciseType ?: ""} || '%' OR :#{#search.exerciseType ?: ""} IS NULL)
+            AND (tp.purpose LIKE '%' || :#{#search.therapeuticPurpose ?: ""} || '%' OR :#{#search.therapeuticPurpose ?: ""} IS NULL)
         ORDER BY ex.title
         LIMIT :pageSize OFFSET :offset
     """
@@ -44,9 +44,9 @@ interface ExercisesRepo : CrudRepository<Exercise, Long>, PagingAndSortingReposi
             INNER JOIN exercise_purposes ep ON ex.id = ep.exercise_id
             INNER JOIN therapeutic_purposes tp ON tp.id = ep.purpose_id
             INNER JOIN exercise_types et on ex.exercise_type_id = et.id
-        WHERE (:#{#search?.title ?: ""} IS NULL OR ex.title LIKE '%' || :#{#search?.title ?: ""} || '%')
-            AND (ex.contradictions LIKE '%' || :#{#search?.contradiction ?: ""} || '%' OR :#{#search?.contradiction ?: ""} IS NULL)
-            AND (:#{#search?.duration ?: ""}::interval IS NULL OR ex.duration = :#{#search?.duration ?: ""}::interval)
+        WHERE (ex.title LIKE '%' || :#{#search.title ?: ""} || '%' OR :#{#search.title ?: ""} IS NULL)
+            AND (ex.contradictions LIKE '%' || :#{#search?.contradiction ?: ""} || '%' OR :#{#search.contradiction ?: ""} IS NULL)
+            AND (:#{#search.duration} IS NULL OR ex.duration = :#{#search.duration}::interval)
             AND (et.name LIKE '%' || :#{#search?.exerciseType ?: ""} || '%' OR :#{#search?.exerciseType ?: ""} IS NULL)
             AND (tp.purpose LIKE '%' || :#{#search?.therapeuticPurpose ?: ""} || '%' OR :#{#search?.therapeuticPurpose ?: ""} IS NULL)
     """
