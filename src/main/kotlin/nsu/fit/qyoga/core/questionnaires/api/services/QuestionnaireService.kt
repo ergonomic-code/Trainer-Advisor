0662904e17b2a7e0c7b2a2d77df64b847c080939
zertalian <Zertalian@mail.ru>
2023-03-14 20:02:22 +0700

    добавил сервис для изображений, обновил сервис опросников, добавив возможность добавлять новые

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/QuestionnaireService.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/QuestionnaireService.kt
@@ -1,7 +1,9 @@
-package nsu.fit.qyoga.core.questionnaires.api
+package nsu.fit.qyoga.core.questionnaires.api.services
 
+import nsu.fit.qyoga.core.questionnaires.api.dtos.CreateQuestionnaireDto
 import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireDto
 import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireSearchDto
+import nsu.fit.qyoga.core.questionnaires.api.model.Questionnaire
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
@@ -10,6 +12,8 @@ interface QuestionnaireService {
         questionnaireSearchDto: QuestionnaireSearchDto,
         pageable: Pageable
     ): Page<QuestionnaireDto>
+
+    fun createQuestionnaire(createQuestionnaireDto: CreateQuestionnaireDto): Questionnaire
 
     fun getQuestionnairesCount(title: String?): Long
 }
---
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/ImagesRepo.kt
@@ -0,0 +1,10 @@
+package nsu.fit.qyoga.core.questionnaires.internal
+
+import nsu.fit.qyoga.core.questionnaires.api.model.Image
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ImagesRepo: CrudRepository<Image, Long>, PagingAndSortingRepository<Image, Long> {
+}
