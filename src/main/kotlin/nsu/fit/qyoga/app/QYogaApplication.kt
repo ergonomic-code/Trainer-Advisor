8167a7e0dc5f7dbbdf6e42a8848370bc5c17ee8f
Aleksey Zhidkov <me@azhidkov.pro>
2023-03-14 08:56:20 +0700

    feat: реализована страница просмотра списка опросников

--- src/main/kotlin/nsu/fit/qyoga/app/QYogaApplication.kt
+++ src/main/kotlin/nsu/fit/qyoga/app/QYogaApplication.kt
@@ -1,12 +1,13 @@
 package nsu.fit.qyoga.app
 
 import nsu.fit.qyoga.core.exercises.ExercisesConfig
+import nsu.fit.qyoga.core.questionnaires.QuestionnairesConfig
 import nsu.fit.qyoga.core.users.UsersConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
-@Import(UsersConfig::class, ExercisesConfig::class)
+@Import(UsersConfig::class, ExercisesConfig::class, QuestionnairesConfig::class)
 @SpringBootApplication
 class QYogaApplication
 
---
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionnaireSearchDto.kt
@@ -0,0 +1,6 @@
+package nsu.fit.qyoga.core.questionnaires.api.dtos
+
+data class QuestionnaireSearchDto(
+    val title: String? = null,
+    val orderType: String = "ASC"
+)
