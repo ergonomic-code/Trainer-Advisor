df11b567e2022a5391793ec65f7f8eef7b92b4f7
Aleksey Zhidkov <me@azhidkov.pro>
2023-03-14 08:56:20 +0700

    feat: реализована страница просмотра списка опросников

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/QuestionnaireServiceImpl.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/QuestionnaireServiceImpl.kt
@@ -1,21 +1,14 @@
 package nsu.fit.qyoga.core.questionnaires.internal
 
-import nsu.fit.qyoga.core.questionnaires.api.dtos.CreateQuestionnaireDto
-import nsu.fit.qyoga.core.questionnaires.api.dtos.ImageDto
-import nsu.fit.qyoga.core.questionnaires.api.services.QuestionnaireService
+import nsu.fit.qyoga.core.questionnaires.api.QuestionnaireService
 import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireDto
 import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireSearchDto
-import nsu.fit.qyoga.core.questionnaires.api.model.Questionnaire
-import nsu.fit.qyoga.core.questionnaires.api.services.ImagesService
-import nsu.fit.qyoga.core.questionnaires.api.services.QuestionService
 import org.springframework.data.domain.*
 import org.springframework.stereotype.Service
 
 @Service
 class QuestionnaireServiceImpl(
-    private val questionnaireRepo: QuestionnaireRepo,
-    private val questionService: QuestionService,
-    private val imagesService: ImagesService
+    private val questionnaireRepo: QuestionnaireRepo
 ) : QuestionnaireService {
 
     override fun findQuestionnaires(
@@ -36,25 +29,6 @@ class QuestionnaireServiceImpl(
         }
         val count = getQuestionnairesCount(questionnaireSearchDto.title)
         return PageImpl(entities.map { QuestionnaireDto(id = it.id, title = it.title) }.toList(), pageRequest, count)
-    }
-
-    override fun createQuestionnaire(createQuestionnaireDto: CreateQuestionnaireDto): Questionnaire {
-        val questionnaire = Questionnaire(
-            title = createQuestionnaireDto.title?: "unnamed"
-        )
-        val savedQuestionnaire = questionnaireRepo.save(questionnaire)
-        createQuestionnaireDto.questions.map {
-            val imageId = imagesService.uploadImage(
-                ImageDto(
-                    it.photo?.name,
-                    it.photo?.contentType?: "application/octet-stream",
-                    it.photo!!.size,
-                    it.photo.inputStream
-                )
-            )
-            questionService.createQuestion(it)
-        }
-        return questionnaire
     }
 
     override fun getQuestionnairesCount(title: String?): Long {
