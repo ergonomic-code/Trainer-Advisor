b6b9290a184d670e5e3af63d75064dc63605c188
Aleksey Zhidkov <me@azhidkov.pro>
2023-03-14 08:56:20 +0700

    feat: реализована страница просмотра списка опросников

---
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/QuestionnaireService.kt
@@ -0,0 +1,15 @@
+package nsu.fit.qyoga.core.questionnaires.api
+
+import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireDto
+import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireSearchDto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface QuestionnaireService {
+    fun findQuestionnaires(
+        questionnaireSearchDto: QuestionnaireSearchDto,
+        pageable: Pageable
+    ): Page<QuestionnaireDto>
+
+    fun getQuestionnairesCount(title: String?): Long
+}
