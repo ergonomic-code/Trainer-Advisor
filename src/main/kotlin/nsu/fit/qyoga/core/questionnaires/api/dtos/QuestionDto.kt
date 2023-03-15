fac99d565a35e876824532b43bea13bdcc52330d
zertalian <Zertalian@mail.ru>
2023-03-15 15:10:54 +0700

    добавил репозиторий для вопросов и имплемент сервиса для него же, убрал лишние поля из класса "вопрос"

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionDto.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionDto.kt
@@ -4,7 +4,7 @@ import nsu.fit.qyoga.core.questionnaires.api.enums.QuestionType
 import org.springframework.web.multipart.MultipartFile
 
 data class QuestionDto(
-    val text: String,
+    val text: String?,
     val questionType: QuestionType,
     val photo: MultipartFile?
 )
