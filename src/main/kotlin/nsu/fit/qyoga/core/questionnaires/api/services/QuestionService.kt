52f6328980ee25dc4ebdd07a218c5b96bca6d8c6
zertalian <Zertalian@mail.ru>
2023-03-15 15:10:54 +0700

    добавил репозиторий для вопросов и имплемент сервиса для него же, убрал лишние поля из класса "вопрос"

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/QuestionService.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/QuestionService.kt
@@ -3,8 +3,7 @@ package nsu.fit.qyoga.core.questionnaires.api.services
 import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionDto
 import org.springframework.stereotype.Service
 
-@Service
 interface QuestionService {
 
-    fun createQuestion(questionDto: QuestionDto)
+    fun createQuestion(questionDto: QuestionDto, questionnaireId: Long, imageId: Long)
 }
