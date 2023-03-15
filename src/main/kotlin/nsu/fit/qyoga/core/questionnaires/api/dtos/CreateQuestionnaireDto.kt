fd5d355c4f8c289d0e3b1d4709e8d47f93bbd600
zertalian <Zertalian@mail.ru>
2023-03-15 16:06:50 +0700

    написан скрипт для инита таблицы вопросов, написан тест на сохранения опросника без вопросов

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/CreateQuestionnaireDto.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/CreateQuestionnaireDto.kt
@@ -1,7 +1,6 @@
 package nsu.fit.qyoga.core.questionnaires.api.dtos
 
 data class CreateQuestionnaireDto(
-    val id: Long,
-    val title: String?,
+    val title: String,
     val questions: List<QuestionDto>
 )
