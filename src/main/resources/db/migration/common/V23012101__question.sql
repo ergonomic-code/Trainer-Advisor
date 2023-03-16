27c59f781778408fcbd65ba891c9e24f0d364cd7
zertalian <Zertalian@mail.ru>
2023-03-17 00:59:10 +0700

    Добавил скрипт инита таблицы ответов

--- src/main/resources/db/migration/common/V23012101__question.sql
+++ src/main/resources/db/migration/common/V23012101__question.sql
@@ -5,6 +5,6 @@ CREATE TABLE questions
     question_type VARCHAR,
     questionnaire_id BIGINT NOT NULL,
     image_id BIGINT,
-    FOREIGN KEY (questionnaire_id) REFERENCES questionnaires (id) ON DELETE CASCADE ,
+    FOREIGN KEY (questionnaire_id) REFERENCES questionnaires (id) ON DELETE CASCADE,
     FOREIGN KEY (image_id) REFERENCES images (id) ON DELETE SET NULL
 )
