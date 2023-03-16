6e7499bd8806362518012c602b809aee38c0513c
zertalian <Zertalian@mail.ru>
2023-03-17 00:59:10 +0700

    Добавил скрипт инита таблицы ответов

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/repository/QuestionnaireRepo.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/repository/QuestionnaireRepo.kt
@@ -1,9 +1,11 @@
 package nsu.fit.qyoga.core.questionnaires.internal.repository
 
+import nsu.fit.qyoga.core.questionnaires.api.dtos.QuestionnaireDto
 import nsu.fit.qyoga.core.questionnaires.api.model.Questionnaire
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
@@ -15,6 +17,18 @@ interface QuestionnaireRepo : CrudRepository<Questionnaire, Long>, PagingAndSort
 
     fun countAllByTitleContaining(title: String): Long
     fun findAllByTitleContaining(title: String, pageable: Pageable): List<Questionnaire>
+
+    @Query(
+        """
+           SELECT *
+        FROM questionnaires ques
+            LEFT JOIN questions quns ON ques.id = quns.questionnaire_id
+            LEFT JOIN images imgs ON imgs.id = quns.image_id
+            LEFT JOIN answers anrs ON ques.id = quns.question_id
+        WHERE ques.id = :id
+        """
+    )
+    fun getQuestionnaireWithQuestionById(id: Long): List<QuestionnaireDto>
 }
 fun QuestionnaireRepo.findPageByTitle(title: String, pageable: Pageable): Page<Questionnaire> {
     val entities = this.findAllByTitleContaining(title, pageable)
