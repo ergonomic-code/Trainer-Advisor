1dfdf14136770ad8ce7d0ccbec3fe029642264a0
nntrifonova <n.trifonova@g.nsu.ru>
2023-03-20 00:23:32 +0700

    fix: тесты не проходят

--- src/main/kotlin/nsu/fit/qyoga/app/QYogaApplication.kt
+++ src/main/kotlin/nsu/fit/qyoga/app/QYogaApplication.kt
@@ -1,14 +1,17 @@
 package nsu.fit.qyoga.app
 
+import nsu.fit.qyoga.core.clients.ClientConfig
 import nsu.fit.qyoga.core.exercises.ExercisesConfig
+import nsu.fit.qyoga.core.questionnaires.QuestionnairesConfig
 import nsu.fit.qyoga.core.users.UsersConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
-@Import(UsersConfig::class, ExercisesConfig::class)
+@Import(UsersConfig::class, ExercisesConfig::class, QuestionnairesConfig::class, ClientConfig::class)
 @ComponentScan("nsu.fit.qyoga.core.clients")
+@ComponentScan("nsu.fit.qyoga.core.users")
 @SpringBootApplication
 class QYogaApplication
 
