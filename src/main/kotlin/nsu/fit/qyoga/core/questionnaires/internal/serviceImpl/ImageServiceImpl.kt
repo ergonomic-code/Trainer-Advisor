214ea7b76fb6c45597086fa4e2af8a31ac402355
zertalian <Zertalian@mail.ru>
2023-03-22 22:22:17 +0700

    Удалил старый репозиторий для изображений, заменив на JdbcTemplate, добавил класс для тестов сервиса изображений

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/serviceImpl/ImageServiceImpl.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/serviceImpl/ImageServiceImpl.kt
@@ -2,26 +2,26 @@ package nsu.fit.qyoga.core.questionnaires.internal.serviceImpl
 
 import nsu.fit.qyoga.core.questionnaires.api.dtos.ImageDto
 import nsu.fit.qyoga.core.questionnaires.api.model.Image
-import nsu.fit.qyoga.core.questionnaires.api.services.ImagesService
-import nsu.fit.qyoga.core.questionnaires.internal.repository.ImagesRepo
+import nsu.fit.qyoga.core.questionnaires.api.services.ImageService
+import nsu.fit.qyoga.core.questionnaires.internal.repository.ImageJdbcTemplateRepo
 import org.springframework.stereotype.Service
 
 @Service
 class ImageServiceImpl(
-    private val imagesRepo: ImagesRepo
-) : ImagesService {
+    private val imageRepo: ImageJdbcTemplateRepo
+) :ImageService {
     override fun uploadImage(image: ImageDto): Long {
-        return imagesRepo.save(
+        return imageRepo.save(
             Image(
                 name = image.name ?: "",
                 mediaType = image.mediaType,
                 size = image.size,
                 data = image.data.readAllBytes()
             )
-        ).id
+        )
     }
 
     override fun getImage(id: Long): Image? {
-        return imagesRepo.findById(id).map { it }.orElse(null)
+        return imageRepo.findById(id).map { it }.orElse(null)
     }
 }
