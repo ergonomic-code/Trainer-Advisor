29a4f946ecde7963a5ca9dea9b1be491138a33bf
zertalian <Zertalian@mail.ru>
2023-03-24 21:52:05 +0700

    в create-questionnaire.html добавил кнопку добавления нового вопроса, добавил кнопку отправки сохранения опроса (по совместительству "задать баллы"), добавил в сервисы Опросников и Вопросов возможность создавать пустые экземпляры, теперь при попытке создать новый опросник он будет создаваться сразу, что позволит сохранять результаты работы без необходимости ручного сохранения

---
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionDto.kt
@@ -0,0 +1,11 @@
+package nsu.fit.qyoga.core.questionnaires.api.dtos
+
+import nsu.fit.qyoga.core.questionnaires.api.enums.QuestionType
+
+data class QuestionDto(
+    val id: Long,
+    val title: String?,
+    val questionType: QuestionType,
+    val questionnaireId: Long,
+    val imageId: Long?
+)
