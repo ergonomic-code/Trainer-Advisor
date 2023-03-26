42c69ad6a72fba9e6ee2d3da7f26fdf1d71b7b59
zertalian <Zertalian@mail.ru>
2023-03-26 15:30:30 +0700

    почистил дтошки, добавил страницу с ошибкой, добавил исключение для опросников, переработал сервисы и ошибки в соответствии с дтошками, добавил поиск изображения в репозитории. Переработал контроллер создания опросника

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/ImageService.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/ImageService.kt
@@ -1,10 +1,9 @@
 package nsu.fit.qyoga.core.questionnaires.api.services
 
-import nsu.fit.qyoga.core.questionnaires.api.dtos.ImageDto
 import nsu.fit.qyoga.core.questionnaires.api.model.Image
 
 interface ImageService {
-    fun uploadImage(image: ImageDto): Long
+    fun uploadImage(image: Image): Long
 
     fun getImage(id: Long): Image?
 }
