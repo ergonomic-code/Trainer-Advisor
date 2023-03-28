63f83704f6f79bb979dea1b0c539ff3029012a14
zertalian <Zertalian@mail.ru>
2023-03-28 23:37:23 +0700

    дабавил ошибку для вопросов, изменил от кого наследуется ошибка опросников, поменял скрипты инита ответов и вопросов добавив ON DELETE CASCADE, сделал создание и удаление вопроса из опросника пофиксил фронт для корректной работы thymeleaf

--- src/main/resources/db/migration/common/V23012401__questions.sql
+++ src/main/resources/db/migration/common/V23012401__questions.sql
@@ -3,6 +3,6 @@ CREATE TABLE questions
     id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT,
     question_type VARCHAR,
-    questionnaire_id BIGINT NOT NULL REFERENCES questionnaires ON DELETE NO ACTION ,
+    questionnaire_id BIGINT NOT NULL REFERENCES questionnaires ON DELETE CASCADE ,
     image_id BIGINT REFERENCES images ON DELETE NO ACTION
 )
