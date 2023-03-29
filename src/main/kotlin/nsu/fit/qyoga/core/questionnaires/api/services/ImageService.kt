7516936c975d7f071d0e1c1fbd8cb38d73cd6d09
zertalian <Zertalian@mail.ru>
2023-03-29 22:25:26 +0700

    Добавил ошибку для изображений, поправил скрипты инита вопросов и ответов. Задал максимальный размер загружаемого изображения, добавил добавление изображения для опросника, добавил отображение изображения для опросника (фронт и бэк)

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/ImageService.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/services/ImageService.kt
@@ -1,9 +1,10 @@
 package nsu.fit.qyoga.core.questionnaires.api.services
 
 import nsu.fit.qyoga.core.questionnaires.api.model.Image
+import org.springframework.web.multipart.MultipartFile
 
 interface ImageService {
-    fun uploadImage(image: Image): Long
-
-    fun getImage(id: Long): Image?
+    fun uploadImage(file: MultipartFile): Long
+    fun deleteImage(id: Long)
+    fun getImage(id: Long): Image
 }
--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/serviceImpl/ImageServiceImpl.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/internal/serviceImpl/ImageServiceImpl.kt
@@ -1,26 +1,35 @@
 package nsu.fit.qyoga.core.questionnaires.internal.serviceImpl
 
+import nsu.fit.qyoga.core.questionnaires.api.errors.ImageException
 import nsu.fit.qyoga.core.questionnaires.api.model.Image
 import nsu.fit.qyoga.core.questionnaires.api.services.ImageService
 import nsu.fit.qyoga.core.questionnaires.internal.repository.ImageJdbcTemplateRepo
+import nsu.fit.qyoga.core.questionnaires.internal.repository.ImageRepo
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ImageServiceImpl(
-    private val imageRepo: ImageJdbcTemplateRepo
+    private val imageTemplateRepo: ImageJdbcTemplateRepo,
+    private val imageRepo: ImageRepo
 ) :ImageService {
-    override fun uploadImage(image: Image): Long {
-        return imageRepo.save(
+    override fun uploadImage(file: MultipartFile): Long {
+        return imageTemplateRepo.save(
             Image(
-                name = image.name,
-                mediaType = image.mediaType,
-                size = image.size,
-                data = image.data
+                name = file.originalFilename ?: throw ImageException("Ошибка при сохранении: ошибка в имени файла"),
+                mediaType = file.contentType ?: "application/octet-stream",
+                size = file.size,
+                data = file.bytes
             )
         )
     }
 
-    override fun getImage(id: Long): Image? {
-        return imageRepo.findById(id)
+    override fun deleteImage(id: Long) {
+        imageRepo.deleteById(id)
+    }
+
+    override fun getImage(id: Long): Image {
+        return imageRepo.findById(id).orElse(null)
+            ?: throw ImageException("Выбранное изображение не найдено")
     }
 }
