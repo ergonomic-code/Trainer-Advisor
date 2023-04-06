f9750b8974e0e6834ae49b3c2cf6109596eba17d
zertalian <Zertalian@mail.ru>
2023-04-06 09:59:29 +0700

    починил отображение ответа, починил сохранение

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionWithAnswersDto.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionWithAnswersDto.kt
@@ -3,10 +3,10 @@ package nsu.fit.qyoga.core.questionnaires.api.dtos
 import nsu.fit.qyoga.core.questionnaires.api.dtos.enums.QuestionType
 
 data class QuestionWithAnswersDto(
-    val id: Long = 0,
+    var id: Long = 0,
     val title: String? = null,
-    val questionType: QuestionType = QuestionType.SINGLE,
-    val imageId: Long? = null,
-    val questionnaireId: Long = 0,
+    var questionType: QuestionType = QuestionType.SINGLE,
+    var imageId: Long? = null,
+    var questionnaireId: Long = 0,
     var answers: List<AnswerDto> = mutableListOf()
 )
