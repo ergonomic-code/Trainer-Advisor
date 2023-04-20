f7db90ab96c07acaa8111e9b27162a07b4730977
zertalian <Zertalian@mail.ru>
2023-04-20 14:12:07 +0700

    Правки стиля: сделал все классы дата классами

--- src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionnaireWithQuestionDto.kt
+++ src/main/kotlin/nsu/fit/qyoga/core/questionnaires/api/dtos/QuestionnaireWithQuestionDto.kt
@@ -1,7 +1,7 @@
 package nsu.fit.qyoga.core.questionnaires.api.dtos
 
 data class QuestionnaireWithQuestionDto(
-    val id: Long,
-    val title: String,
+    var id: Long,
+    var title: String,
     var questions: List<QuestionWithAnswersDto> = mutableListOf()
 )
