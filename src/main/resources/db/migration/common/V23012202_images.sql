8d9f0f954be4cbe59b44e9c31cf91c0343a898ec
zertalian <Zertalian@mail.ru>
2023-05-03 15:15:10 +0700

    Удалил лишние файлы

--- src/main/resources/db/migration/common/V23012202_images.sql
+++
@@ -1,8 +0,0 @@
-create table images
-(
-    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name       VARCHAR,
-    media_type VARCHAR NOT NULL,
-    size       BIGINT,
-    data       BYTEA
-);
