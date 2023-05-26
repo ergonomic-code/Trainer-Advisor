31e2ddbc2daa9d5a498f332e1513a743909c79b9
zertalian <Zertalian@mail.ru>
2023-05-26 11:21:57 +0700

    Пофиксил страницы

--- src/main/resources/db/migration/demo/V230522001__insert_completing_data.sql
+++ src/main/resources/db/migration/demo/V230522001__insert_completing_data.sql
@@ -6,18 +6,18 @@ VALUES
 INSERT INTO completing
     (completing_date, numeric_result, text_result, questionnaire_id, client_id, therapist_id)
 VALUES
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, 1),
-    ('2023-06-22', 10, 'large_text_result_123456789011121314151617181920', 2, 2, 1),
-    ('2023-07-22', 10, 'large_text_result_123456789011121314151617181920', 3, 1, 1),
-    ('2023-08-22', 10, 'large_text_result_123456789011121314151617181920', 4, 1, 1),
-    ('2023-09-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, 1),
-    ('2023-01-22', 10, 'large_text_result_123456789011121314151617181920', 6, 1, 1),
-    ('2023-02-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, 1),
-    ('2023-03-22', 10, 'large_text_result_123456789011121314151617181920', 10, 2, 1),
-    ('2023-04-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, 1),
-    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, 1)
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-06-22', 10, 'large_text_result_123456789011121314151617181920', 2, 2, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-07-22', 10, 'large_text_result_123456789011121314151617181920', 3, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-08-22', 10, 'large_text_result_123456789011121314151617181920', 4, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-09-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-01-22', 10, 'large_text_result_123456789011121314151617181920', 6, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-02-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-03-22', 10, 'large_text_result_123456789011121314151617181920', 10, 2, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-04-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 9, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова')),
+    ('2023-05-22', 10, 'large_text_result_123456789011121314151617181920', 1, 1, (SELECT id from therapists WHERE name = 'Екатерина Маркова'))
