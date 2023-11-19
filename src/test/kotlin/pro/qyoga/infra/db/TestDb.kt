ad507395ab07c5161adff634eb65cfb4a9134c31
Dmitriy <golenko_dmitriy@mail.ru>
2023-11-20 06:18:45 +0700

    env/qg-80 В docker-compose файлы добавил контейнер minio, в тестовой инфраструктуре попытался повторить трюк с postgres, все ран конфиги продолжают работать

--- src/test/kotlin/pro/qyoga/infra/db/TestDb.kt
+++ src/test/kotlin/pro/qyoga/infra/db/TestDb.kt
@@ -1,12 +1,15 @@
 package pro.qyoga.infra.db
 
+import io.minio.MinioClient
 import org.flywaydb.core.Flyway
 import org.postgresql.ds.PGSimpleDataSource
 import org.slf4j.LoggerFactory
+import java.net.ConnectException
 import java.sql.DriverManager
 import java.sql.SQLException
 
 const val PROVIDED_DB_URL = "jdbc:postgresql://localhost:54502/qyoga"
+const val MINIO_URL = "http://localhost:50001"
 
 object TestDb
 
@@ -15,9 +18,16 @@ private val log = LoggerFactory.getLogger(TestDb::class.java)
 private const val DB_USER = "postgres"
 private const val DB_PASSWORD = "password"
 
+private const val MINIO_USER = "user"
+private const val MINIO_PASSWORD = "password"
+
 val jdbcUrl: String by lazy {
     try {
-        val con = DriverManager.getConnection(PROVIDED_DB_URL.replace("qyoga", DB_USER), DB_USER, DB_PASSWORD)
+        val con = DriverManager.getConnection(
+            PROVIDED_DB_URL.replace("qyoga", DB_USER),
+            DB_USER,
+            DB_PASSWORD
+        )
         log.info("Provided db found, recreating it")
         con.prepareStatement(
             """
@@ -33,6 +43,25 @@ val jdbcUrl: String by lazy {
         pgContainer.jdbcUrl
     }
 }
+
+val minioUrl: String by lazy {
+    try {
+        val con = MinioClient.builder()
+            .endpoint(MINIO_URL)
+            .credentials(MINIO_USER, MINIO_PASSWORD)
+            .build()
+        // Не нашел нормального способа проверить наличие соединения
+        con.listBuckets()
+        log.info("minio container found")
+        MINIO_URL
+    } catch (e: ConnectException) {
+        log.info("minio container not found: ${e.message}")
+        log.info("http://" + minioContainer.host + ":" + minioContainer.firstMappedPort)
+        "http://" + minioContainer.host + ":" + minioContainer.firstMappedPort
+    }
+
+}
+
 
 val testDataSource by lazy {
     migrateSchema(jdbcUrl)
