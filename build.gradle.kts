8e4b48b3745a3e998eb933ff26767ee038cd1cd4
Aleksey Zhidkov <me@azhidkov.pro>
2023-11-26 07:52:36 +0700

    build/qg-72: Spring Boot обновлён до версии 3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.10"
 	kotlin("plugin.spring") version "1.9.10"
 	id("io.gitlab.arturbosch.detekt") version "1.23.3"
