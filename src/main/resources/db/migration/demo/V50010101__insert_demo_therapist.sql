5ed499dbfae853983075037aa0f5869b3a5b5c75
Aleksey Zhidkov <me@azhidkov.pro>
2023-12-03 18:11:56 +0700

    ui/qg-103: в UI QYoga заменена на Trainer Advisor

--- src/main/resources/db/migration/demo/V50010101__insert_demo_therapist.sql
+++ src/main/resources/db/migration/demo/V50010101__insert_demo_therapist.sql
@@ -1,13 +1,13 @@
 -- password: "password"
 INSERT INTO users (email, password_hash, roles, created_at, version)
-VALUES ('therapist@qyoga.pro',
+VALUES ('therapist@trainer-advisor.pro',
         '$2a$12$TbB4qOKdLj8lJ1Ml1SlZ.eG/mQGjQ4Ehrz20CjHovlEG2D6QBwLMe',
         '{"ROLE_THERAPIST"}',
         now(),
         1);
 
 INSERT INTO therapists (id, first_name, last_name, created_at, version)
-VALUES ((SELECT id FROM users WHERE email = 'therapist@qyoga.pro'),
+VALUES ((SELECT id FROM users WHERE email = 'therapist@trainer-advisor.pro'),
         'Елена', 'Маркова',
         now(),
         1);
