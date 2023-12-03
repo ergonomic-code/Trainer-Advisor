32381eeb2920284cf1206c6732d35401cbfadf6e
Aleksey Zhidkov <me@azhidkov.pro>
2023-12-03 14:12:07 +0700

    feat/qg-81: включена функция remember me

--- src/main/kotlin/pro/qyoga/app/auth/WebSecurityConfig.kt
+++ src/main/kotlin/pro/qyoga/app/auth/WebSecurityConfig.kt
@@ -8,14 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository
 import pro.qyoga.core.users.UsersConfig
 import pro.qyoga.core.users.api.Role
 import pro.qyoga.infra.auth.AuthConfig
+import java.time.Duration
+import javax.sql.DataSource
 
 
 @Import(AuthConfig::class, UsersConfig::class)
 @Configuration
-class WebSecurityConfig {
+class WebSecurityConfig(
+    private val dataSource: DataSource
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -36,7 +42,20 @@ class WebSecurityConfig {
                     .permitAll()
             }
             .logout { logout: LogoutConfigurer<HttpSecurity?> -> logout.permitAll() }
+            .rememberMe { rememberMeConfigurer ->
+                rememberMeConfigurer
+                    .alwaysRemember(true)
+                    .tokenRepository(tokenRepository())
+                    .tokenValiditySeconds(Duration.ofDays(9).toSeconds().toInt())
+            }
         return http.build()
+    }
+
+    @Bean
+    fun tokenRepository(): PersistentTokenRepository {
+        val jdbcTokenRepositoryImpl = JdbcTokenRepositoryImpl()
+        jdbcTokenRepositoryImpl.setDataSource(dataSource)
+        return jdbcTokenRepositoryImpl
     }
 
 }
--- src/test/kotlin/pro/qyoga/cases/app/auth/AuthTests.kt
+++ src/test/kotlin/pro/qyoga/cases/app/auth/AuthTests.kt
@@ -1,11 +1,21 @@
 package pro.qyoga.cases.app.auth
 
 import io.restassured.http.Cookie
+import io.restassured.matcher.RestAssuredMatchers.detailedCookie
+import io.restassured.module.kotlin.extensions.Then
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
 import pro.qyoga.assertions.shouldBe
+import pro.qyoga.clients.PublicClient
 import pro.qyoga.clients.TherapistClient
 import pro.qyoga.clients.pages.publc.LoginPage
+import pro.qyoga.fixture.therapists.THE_THERAPIST_LOGIN
+import pro.qyoga.fixture.therapists.THE_THERAPIST_PASSWORD
 import pro.qyoga.infra.web.QYogaAppBaseTest
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.*
 
 
 class AuthTests : QYogaAppBaseTest() {
@@ -20,6 +30,23 @@ class AuthTests : QYogaAppBaseTest() {
 
         // Then
         response shouldBe LoginPage
+    }
+
+    @Test
+    fun `TA should remember users for 9 days`() {
+        // Given
+        val now = Instant.now()
+
+        // When
+        val response = PublicClient.authApi.loginForVerification(THE_THERAPIST_LOGIN, THE_THERAPIST_PASSWORD)
+
+        response.Then {
+            statusCode(HttpStatus.FOUND.value())
+            cookie(
+                "remember-me", detailedCookie()
+                    .expiryDate(Matchers.greaterThan(Date(now.plus(9, ChronoUnit.DAYS).toEpochMilli())))
+            )
+        }
     }
 
 }
