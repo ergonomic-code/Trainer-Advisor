614716e6412b42c3fec8af51dc8f1c5216523795
Aleksey Zhidkov <me@azhidkov.pro>
2023-12-26 16:09:26 +0700

    refactor/qg-117: тест левой навпанели переведён на работу через компоненты

---
+++ src/test/kotlin/pro/qyoga/tests/clients/pages/therapist/fragments/LeftNavBarFragment.kt
@@ -0,0 +1,26 @@
+package pro.qyoga.tests.clients.pages.therapist.fragments
+
+import org.jsoup.nodes.Element
+import pro.qyoga.tests.assertions.shouldHaveComponent
+import pro.qyoga.tests.clients.pages.therapist.clients.ClientsListPage
+import pro.qyoga.tests.clients.pages.therapist.therapy.exercises.ExercisesListPage
+import pro.qyoga.tests.infra.html.Component
+import pro.qyoga.tests.infra.html.Link
+
+object LeftNavBarFragment : Component {
+
+    override fun selector() = "#layoutSidenav_nav"
+
+    private val clientsLink = Link("clientsPageLink", ClientsListPage, "Клиенты")
+    private val exercisesLink = Link("exercisesPageLink", ExercisesListPage, "Упражнения")
+
+    private val logoutLink = Link("logoutLink", "/logout", "Выйти")
+
+    override fun match(element: Element) {
+        element shouldHaveComponent clientsLink
+        element shouldHaveComponent exercisesLink
+
+        element shouldHaveComponent logoutLink
+    }
+
+}
