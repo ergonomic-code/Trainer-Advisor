34b575f9cf37cb67cea5ff3ab78dbd845d091892
Aleksey Zhidkov <me@azhidkov.pro>
2023-12-26 18:15:14 +0700

    feat/qg-117: в левое меню добавлена ссылка на страницу терапевтических задач

--- src/test/kotlin/pro/qyoga/tests/clients/pages/therapist/fragments/LeftNavBarFragment.kt
+++ src/test/kotlin/pro/qyoga/tests/clients/pages/therapist/fragments/LeftNavBarFragment.kt
@@ -4,6 +4,7 @@ import org.jsoup.nodes.Element
 import pro.qyoga.tests.assertions.shouldHaveComponent
 import pro.qyoga.tests.clients.pages.therapist.clients.ClientsListPage
 import pro.qyoga.tests.clients.pages.therapist.therapy.exercises.ExercisesListPage
+import pro.qyoga.tests.clients.pages.therapist.therapy.therapeutic_tasks.TherapeuticTasksListPage
 import pro.qyoga.tests.infra.html.Component
 import pro.qyoga.tests.infra.html.Link
 
@@ -14,11 +15,16 @@ object LeftNavBarFragment : Component {
     private val clientsLink = Link("clientsPageLink", ClientsListPage, "Клиенты")
     private val exercisesLink = Link("exercisesPageLink", ExercisesListPage, "Упражнения")
 
+    private val therapeuticTasksLink =
+        Link("therapeuticTasksPageLink", TherapeuticTasksListPage, "Терапевтические задачи")
+
     private val logoutLink = Link("logoutLink", "/logout", "Выйти")
 
     override fun match(element: Element) {
         element shouldHaveComponent clientsLink
         element shouldHaveComponent exercisesLink
+
+        element shouldHaveComponent therapeuticTasksLink
 
         element shouldHaveComponent logoutLink
     }
