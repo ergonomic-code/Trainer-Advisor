da3b90243b4b66971d150fa2242172d7a129d6c4
Aleksey Zhidkov <me@azhidkov.pro>
2023-12-30 09:03:39 +0700

    feat/qg-118: проект переведён на чистый Twitter Bootstrap 5.3

--- src/main/resources/static/js/closeModal.js
+++
@@ -1,7 +0,0 @@
-function closeModal() {
-    const el = document.getElementById("modal")
-    el.classList.add('closing')
-    el.addEventListener('animationend', function() {
-        el.remove()
-    })
-}
