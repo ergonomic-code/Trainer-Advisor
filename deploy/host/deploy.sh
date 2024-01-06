6d07a173c897397b5730d4cb77e607ef0a46ed0e
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-06 19:14:40 +0700

    feat/qg-80: завершена миграция хранения файлов на минио

--- deploy/host/deploy.sh
+++ deploy/host/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./backup-db.sh
+./backup-minio.sh
 
 docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml pull
 docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml up -d
---
+++ src/main/resources/db/migration/common/V24010602__remove_files_data.sql
@@ -0,0 +1,2 @@
+ALTER TABLE files
+    DROP COLUMN data;
