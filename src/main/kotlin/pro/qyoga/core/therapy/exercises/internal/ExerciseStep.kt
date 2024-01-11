f0ad96e4c14bfd3369e07a8eafc6e5008b4dac8d
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-11 19:22:35 +0700

    refactor/qg-123: ссылка на изображение шага упражнения переведена на AggregateReference

--- src/main/kotlin/pro/qyoga/core/therapy/exercises/internal/ExerciseStep.kt
+++ src/main/kotlin/pro/qyoga/core/therapy/exercises/internal/ExerciseStep.kt
@@ -1,10 +1,12 @@
 package pro.qyoga.core.therapy.exercises.internal
 
+import org.springframework.data.jdbc.core.mapping.AggregateReference
 import org.springframework.data.relational.core.mapping.Table
+import pro.qyoga.platform.file_storage.api.StoredFile
 
 
 @Table("exercise_steps")
 data class ExerciseStep(
     val description: String,
-    val imageId: Long?
+    val imageId: AggregateReference<StoredFile, Long>?
 )
