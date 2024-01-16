75635127073800b3fc534b8c0309ecd41632a600
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-16 18:49:50 +0700

    feat/qg-127: реализована kotlin-friendly нанобиблиотека программного управления транзакциями

---
+++ src/main/kotlin/pro/qyoga/platform/spring/tx/TransactionTemplateExt.kt
@@ -0,0 +1,17 @@
+package pro.qyoga.platform.spring.tx
+
+import org.springframework.transaction.support.TransactionTemplate
+
+interface TransactionalService {
+
+    val transactionTemplate: TransactionTemplate
+
+    fun <T : Any?> transaction(body: () -> T): T {
+        val res = transactionTemplate.execute {
+            body()
+        }
+        @Suppress("UNCHECKED_CAST")
+        return res as T
+    }
+
+}
