e7536a3498f4b3b0dff61f35e1c86add2a4ef945
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-21 12:54:02 +0700

    refactor/qg-133: удалён цикл в пакетах infra <-> platform

--- src/main/kotlin/pro/qyoga/platform/file_storage/FilesStorageConfig.kt
+++ src/main/kotlin/pro/qyoga/platform/file_storage/FilesStorageConfig.kt
@@ -2,10 +2,7 @@ package pro.qyoga.platform.file_storage
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-import pro.qyoga.infra.db.SdjConfig
 
-@Import(SdjConfig::class)
 @ComponentScan
 @Configuration
 class FilesStorageConfig
