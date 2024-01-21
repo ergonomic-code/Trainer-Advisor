0563a834135f1b2ccb1f6ad8317ba2ed42580961
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-21 12:54:02 +0700

    refactor/qg-133: удалён цикл в пакетах infra <-> platform

---
+++ src/test/kotlin/pro/qyoga/tests/cases/arch/ArchTest.kt
@@ -0,0 +1,36 @@
+package pro.qyoga.tests.cases.arch
+
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.library.Architectures
+import org.junit.jupiter.api.Test
+
+
+class ArchTest {
+
+    private val qyogaClasses: JavaClasses = ClassFileImporter()
+        .withImportOption(
+            ImportOption.DoNotIncludeTests()
+        )
+        .importPackages("pro")
+
+    @Test
+    fun `QYoga should conform to ergonomic system architecture`() {
+        val testsAbstractionLayer = "Tests abstraction layer"
+        Architectures.layeredArchitecture()
+            .consideringAllDependencies()
+            .layer(testsAbstractionLayer).definedBy("pro.qyoga.tests.(assertions|fixture|infra)..")
+            .layer("App").definedBy("pro.qyoga.app..")
+            .layer("Core").definedBy("pro.qyoga.core..")
+            .layer("Infra").definedBy("pro.qyoga.infra..")
+            .layer("Platform").definedBy("pro.qyoga.platform..")
+
+            .whereLayer("App").mayOnlyBeAccessedByLayers(testsAbstractionLayer)
+            .whereLayer("Core").mayOnlyBeAccessedByLayers("App", testsAbstractionLayer)
+            .whereLayer("Infra").mayOnlyBeAccessedByLayers("Core", "App", testsAbstractionLayer)
+            .whereLayer("Platform").mayOnlyBeAccessedByLayers("App", "Core", "Infra", testsAbstractionLayer)
+            .check(qyogaClasses)
+    }
+
+}
