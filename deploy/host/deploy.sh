283b073efb2d16d4a271a0caa783663d87ccae1a
Aleksey Zhidkov <me@azhidkov.pro>
2024-01-27 13:00:31 +0700

    ops/qg-142: добавлен скрипт для деплоя приложения без бэкапа хранилищ данных

--- deploy/host/deploy.sh
+++ deploy/host/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 ./backup-db.sh
 ./backup-minio.sh
-
-docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml pull
-docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml up -d
+./deploy-only.sh
