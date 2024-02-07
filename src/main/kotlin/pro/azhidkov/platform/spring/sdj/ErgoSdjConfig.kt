6ec5ad05792ba57a5a3f6f8e81603703caa61eef
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-07 12:19:33 +0700

    refactor/qg-150: добавлена конвертация DbActionExecutionException в DataAccessException

--- src/main/kotlin/pro/azhidkov/platform/spring/sdj/ErgoSdjConfig.kt
+++ src/main/kotlin/pro/azhidkov/platform/spring/sdj/ErgoSdjConfig.kt
@@ -4,20 +4,25 @@ import com.fasterxml.jackson.databind.module.SimpleModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jdbc.core.mapping.AggregateReference
+import pro.azhidkov.platform.spring.sdj.erpo.ErgoPersistenceExceptionTranslator
+import com.fasterxml.jackson.databind.Module as JacksonModule
 
 
 @Configuration
 class ErgoSdjConfig {
 
     @Bean
-    fun aggregateReferenceDeserializer(): com.fasterxml.jackson.databind.Module {
-        return SimpleModule("AggregateReferenceDeserializer").apply {
+    fun aggregateReferenceDeserializer(): JacksonModule =
+        SimpleModule("AggregateReferenceDeserializer").apply {
             addDeserializer(AggregateReference::class.java, AggregateReferenceDeserializer())
         }
-    }
 
     @Bean
     fun aggregateReferenceBindingAdvice() =
         AggregateReferenceBindingAdvice()
+
+    @Bean
+    fun persistenceExceptionTranslator() =
+        ErgoPersistenceExceptionTranslator()
 
 }
