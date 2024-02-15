2a85459d461fb5bf5ac56ef7fd5b0dff294201f3
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-15 17:45:44 +0700

    refactor/qg-166: удалено дублированное определение системной локали

--- src/main/kotlin/pro/qyoga/infra/locale/Locale.kt
+++
@@ -1,5 +0,0 @@
-package pro.qyoga.infra.locale
-
-import java.util.*
-
-val qyogaLocale: Locale = Locale.forLanguageTag("ru-RU")
--- src/main/kotlin/pro/qyoga/infra/timezones/TimeZonesConfig.kt
+++ src/main/kotlin/pro/qyoga/infra/timezones/TimeZonesConfig.kt
@@ -3,13 +3,13 @@ package pro.qyoga.infra.timezones
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import pro.azhidkov.timezones.TimeZones
-import pro.qyoga.infra.locale.qyogaLocale
+import pro.qyoga.l10n.systemLocale
 
 
 @Configuration
 class TimeZonesConfig {
 
     @Bean
-    fun timeZones() = TimeZones(qyogaLocale)
+    fun timeZones() = TimeZones(systemLocale)
 
 }
