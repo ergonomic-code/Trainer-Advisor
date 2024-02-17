04618f6d84ba7fd46f5b2d1b86b021c2424caa54
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-17 19:13:01 +0700

    refactor/qg-164: RegisterUserScenario переведён на испольдзоване Page-объектов

---
+++ src/e2eTest/kotlin/pro/qyoga/tests/platform/selenide/SelenideExt.kt
@@ -0,0 +1,15 @@
+package pro.qyoga.tests.platform.selenide
+
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.SelenideElement
+import com.codeborne.selenide.TypeOptions
+import pro.qyoga.tests.infra.html.Component
+import java.time.Duration
+
+
+fun SelenideElement.fastType(value: String) {
+    this.type(TypeOptions.text(value).withDelay(Duration.ofMillis(10)))
+}
+
+fun `$`(component: Component): SelenideElement =
+    Selenide.`$`(component.selector())
