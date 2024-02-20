746bc218743d9ac2ad2e9d2988dceb6ca4482144
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-20 13:03:57 +0700

    build/qg-166: добавлен файл корневого проекта

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("io.spring.dependency-management") version "1.1.4" apply false
+    id("org.springframework.boot") version "3.2.0" apply false
+}
+
