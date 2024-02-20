53937dce3627d7e4dc47a0077414d113cf36c493
Aleksey Zhidkov <me@azhidkov.pro>
2024-02-20 13:26:06 +0700

    build/qg-166: e2e-тесты выделены в отедльный модуль
    Для того, чтобы починить проблему, что при переключении с е2е на обычные тесты и обратно надо руками удалять out

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.21" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
     id("org.springframework.boot") version "3.2.0" apply false
 }
