f1e157945496d0cb1de18d850d4d13e933d3d00b
Aleksey Zhidkov <me@azhidkov.pro>
2024-03-21 12:40:50 +0700

    ops/qg-xx: В скрипт обновления сертификата добавлен перезапуск nginx-а
    Потому что в прошлый раз скрипт вроде бы отрабатывал без проблем, но сертификат не обновился. Пока обновлял вроде ничего особого не сделал, кроме как рестартанул nginx

--- deploy/host/update-cert.sh
+++ deploy/host/update-cert.sh
@@ -1,1 +1,3 @@
+cd ../server || exit
 ansible-playbook -v -i ./hosts -u root ./qyoga-server.yaml --tags "cert"
+ssh root@trainer-advisor.pro "sudo systemctl restart nginx.service"
--- deploy/host/update-nginx-conf.sh
+++ deploy/host/update-nginx-conf.sh
@@ -1,2 +1,2 @@
-cd ../server
+cd ../server || exit
 ansible-playbook -v -i ./hosts -u root ./qyoga-server.yaml --tags "configure-nginx"
