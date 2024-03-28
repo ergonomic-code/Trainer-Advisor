e9e8c9dfbed805a80671604ea5a33e7b7aeb2788
Aleksey Zhidkov <me@azhidkov.pro>
2024-03-28 19:25:04 +0700

    chore/qg-162: домен продовго сервера в скрипте бэкапа минио исправлен на trainer-advisor.pro

--- deploy/host/backup-minio.sh
+++ deploy/host/backup-minio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Creating minio backup"
 
-rsync -avzP root@qyoga.pro:/var/minio/qyoga/ /data/azhidkov/backups/qyoga/minio
+rsync -avzP root@trainer-advisor.pro:/var/minio/qyoga/ /data/azhidkov/backups/qyoga/minio
