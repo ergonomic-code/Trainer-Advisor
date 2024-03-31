86e012bd92889d038eb1082265d2435e003a6a47
Aleksey Zhidkov <me@azhidkov.pro>
2024-03-31 12:51:18 +0700

    ops/qg-185: добавлено логгирование процесса регистрации терапевта

--- app/src/main/kotlin/pro/qyoga/i9ns/email/EmailSender.kt
+++ app/src/main/kotlin/pro/qyoga/i9ns/email/EmailSender.kt
@@ -1,5 +1,6 @@
 package pro.qyoga.i9ns.email
 
+import org.slf4j.LoggerFactory
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
@@ -9,7 +10,10 @@ class EmailSender(
     private val mailSender: JavaMailSender
 ) {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     fun sendEmail(email: Email) {
+        log.info("Sending ${email.subject} to ${email.to}")
         val mail = mailSender.createMimeMessage().apply {
             MimeMessageHelper(this).apply {
                 setFrom(email.from)
