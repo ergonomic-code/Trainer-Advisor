cf823757b7ea979cd4129eed99a2a7f4c1f309a4
Dmitriy <golenko_dmitriy@mail.ru>
2024-05-25 23:59:20 +0700

    feat/qg-146: Рабочий вариант модального окна

--- app/src/main/kotlin/pro/qyoga/app/therapist/therapy/exercises/EditExercisePageController.kt
+++ app/src/main/kotlin/pro/qyoga/app/therapist/therapy/exercises/EditExercisePageController.kt
@@ -8,8 +8,11 @@ import pro.azhidkov.platform.spring.http.hxRedirect
 import pro.azhidkov.platform.spring.mvc.modelAndView
 import pro.qyoga.app.platform.ResponseEntityExt
 import pro.qyoga.app.platform.notFound
+import pro.qyoga.app.therapist.therapy.exercises.EditExercisePageController.ExerciseStepProcessor.processSteps
 import pro.qyoga.core.therapy.exercises.ExercisesService
 import pro.qyoga.core.therapy.exercises.dtos.ExerciseSummaryDto
+import pro.qyoga.core.therapy.exercises.model.ExerciseStep
+
 
 @Controller
 @RequestMapping("/therapist/exercises/{exerciseId}")
@@ -34,6 +37,55 @@ class EditExercisePageController(
             ?: return notFound
         println(exercise.steps)
         return modelAndView("therapist/therapy/exercises/exercise-modal") {
+            "exercise" bindTo exercise
+        }
+    }
+
+    data class ProcessingExerciseStep(
+        var description: String,
+        var imageUrl: String = NO_IMAGE,
+        var fileName: String? = null
+    ) {
+        fun removeImage() {
+            this.imageUrl = NO_IMAGE
+            this.fileName = null
+        }
+
+        companion object {
+            const val NO_IMAGE = "/img/no-image.png"
+        }
+    }
+
+
+    object ExerciseStepProcessor {
+        const val NO_IMAGE = "/img/no-image.png"
+        fun processSteps(
+            steps: List<ExerciseStep>?,
+            exerciseId: Long
+        ): List<ProcessingExerciseStep> {
+            return if (steps.isNullOrEmpty()) {
+                listOf(ProcessingExerciseStep("", NO_IMAGE))
+            } else {
+                steps.mapIndexed { idx, step ->
+                    val imageUrl =
+                        step.imageId?.let { "/therapist/exercises/$exerciseId/step-images/$idx" }
+                            ?: ProcessingExerciseStep.NO_IMAGE
+                    ProcessingExerciseStep(description = step.description, imageUrl = imageUrl)
+                }
+            }
+        }
+    }
+
+    @GetMapping("/steps")
+    fun getStepsFragment(@PathVariable exerciseId: Long): ModelAndView {
+        val exercise = exercisesService.findById(exerciseId)
+
+        val processedSteps = processSteps(
+            exercise?.steps,
+            exerciseId
+        )
+        return modelAndView("therapist/therapy/exercises/steps") {
+            "steps" bindTo processedSteps
             "exercise" bindTo exercise
         }
     }
