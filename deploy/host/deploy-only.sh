f4abe5264e1c6cf1a869c355f8d8cab6686d7b17
Aleksey Zhidkov <me@azhidkov.pro>
2024-06-19 19:39:41 +0700

    ops/qg-xxx: в скрипты деплоя добавлены гварды на существование файлов вызываемых скриптов

--- deploy/host/deploy-only.sh
+++ deploy/host/deploy-only.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Deploying latest Trainer Advisor version'
-source ./secrets.sh
+source ./secrets.sh || exit
 docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml pull
 docker --context qyoga-prod compose -p qyoga -f ../qyoga/docker-compose.yml up -d
