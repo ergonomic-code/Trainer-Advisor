3bd542118f39a0605eb176c27f5240f9f02fedf8
Aleksey Zhidkov <me@azhidkov.pro>
2025-01-07 17:24:12 +0700

    ops/qg-196: формат имени файлов бэкапов постгреса переведён на yymmdd...

--- deploy/host/backup-db.sh
+++ deploy/host/backup-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Creating db backup"
 
-docker --context qyoga-prod exec -t qyoga-postgres-1 pg_dumpall -c -U postgres > "/data/azhidkov/backups/qyoga/postgres/qyoga_dump_$(date +%d-%m-%Y"_"%H_%M_%S).sql"
+docker --context qyoga-prod exec -t qyoga-postgres-1 pg_dumpall -c -U postgres > "/data/azhidkov/backups/qyoga/postgres/qyoga_dump_$(date +%y%m%d-%H%M%S).sql"
