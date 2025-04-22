60826c686c53e5fce7c42fb243ceaf8597b5b83a
Aleksey Zhidkov <me@azhidkov.pro>
2025-04-22 11:43:25 +0700

    fix/qg-264: включен аудит SDJ-сущностей

--- app/src/main/kotlin/pro/qyoga/infra/db/SdjConfig.kt
+++ app/src/main/kotlin/pro/qyoga/infra/db/SdjConfig.kt
@@ -2,9 +2,11 @@ package pro.qyoga.infra.db
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 import pro.azhidkov.platform.spring.sdj.converters.*
 
 
+@EnableJdbcAuditing
 @Configuration
 class SdjConfig(
     private val modulesConverters: List<ModuleConverters>
