f6294760aebc55504769e399bc551133b3838e96
Aleksey Zhidkov <me@azhidkov.pro>
2025-05-29 10:56:46 +0700

    refactor/qg-273: все внешние зависимости собраны в директории vendor
    Для того того, чтобы их все можно было закешировать одной строкой.
    
    Попутно:
    1. добавлено потерянное кеширование в nginx-е картинок, чтобы страницы быстрее грузились
    2. добавлены именя колонок в запрос месячной статистики по записям журнала, чтобы было удобнее работать с его визуализацией

--- app/src/main/kotlin/pro/qyoga/app/infra/WebSecurityConfig.kt
+++ app/src/main/kotlin/pro/qyoga/app/infra/WebSecurityConfig.kt
@@ -59,7 +59,7 @@ class WebSecurityConfig(
                         "/img/**",
                         "/js/**",
                         "/fonts/**",
-                        "/fontawesome*/**",
+                        "/vendor/**",
                         "/test/*"
                     )
                     .permitAll()
