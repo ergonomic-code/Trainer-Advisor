18fe797162a91383e7740034ac09703032be0bc4
Aleksey Zhidkov <me@azhidkov.pro>
2025-08-01 14:08:37 +0700

    tests/qg-253: в QYogaAppIntegrationBaseKoTest добавлена конфигурация RestAssured
    Для того чтобы в одном тесте можно было использовать и новые методы на базе WebTestClient и старые ещё не переведённые с RestAssured

--- app/src/test/kotlin/pro/qyoga/tests/infra/web/QYogaAppIntegrationBaseKoTest.kt
+++ app/src/test/kotlin/pro/qyoga/tests/infra/web/QYogaAppIntegrationBaseKoTest.kt
@@ -1,10 +1,16 @@
 package pro.qyoga.tests.infra.web
 
+import pro.qyoga.tests.infra.rest_assured.configureRestAssured
+import pro.qyoga.tests.infra.test_config.spring.context
+
 
 abstract class QYogaAppIntegrationBaseKoTest(body: QYogaAppIntegrationBaseKoTest.() -> Unit = {}) :
     QYogaAppBaseKoTest() {
 
     init {
+        beforeSpec {
+            configureRestAssured(context)
+        }
         body()
     }
 
--- app/src/test/kotlin/pro/qyoga/tests/infra/web/QYogaAppIntegrationBaseTest.kt
+++ app/src/test/kotlin/pro/qyoga/tests/infra/web/QYogaAppIntegrationBaseTest.kt
@@ -1,13 +1,8 @@
 package pro.qyoga.tests.infra.web
 
-import io.restassured.RestAssured
-import io.restassured.builder.RequestSpecBuilder
-import io.restassured.config.LogConfig
-import io.restassured.config.RestAssuredConfig
-import io.restassured.http.ContentType
 import org.junit.jupiter.api.BeforeEach
 import pro.qyoga.tests.clients.TherapistClient
-import pro.qyoga.tests.infra.test_config.spring.baseUrl
+import pro.qyoga.tests.infra.rest_assured.configureRestAssured
 import pro.qyoga.tests.infra.test_config.spring.context
 
 
@@ -17,16 +12,7 @@ open class QYogaAppIntegrationBaseTest : QYogaAppBaseTest() {
 
     @BeforeEach
     fun setupRestAssured() {
-        val logConfig = LogConfig.logConfig()
-        val config = RestAssuredConfig.config().logConfig(logConfig)
-
-        RestAssured.requestSpecification = RequestSpecBuilder()
-            .setBaseUri(context.baseUrl)
-            .setAccept(ContentType.HTML)
-            .setContentType("application/x-www-form-urlencoded; charset=UTF-8")
-            .setRelaxedHTTPSValidation()
-            .setConfig(config)
-            .build()
+        configureRestAssured(context)
     }
 
 }
