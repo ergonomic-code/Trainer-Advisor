45f46bb088b973b71cc48f9c7908084c4ec13110
Aleksey Zhidkov <me@azhidkov.pro>
2025-08-02 13:43:30 +0700

    tests/qg-253: WebTestClient переведён на работу по ХТТП
    Для того, чтобы работу через него можно было совмещать с работой через RestAssured. Плюс пока основной контекст поднимает Томкэт эффекта от MockMvc не много

--- app/src/testFixtures/kotlin/pro/qyoga/tests/infra/web/WebTestClient.kt
+++ app/src/testFixtures/kotlin/pro/qyoga/tests/infra/web/WebTestClient.kt
@@ -1,24 +1,14 @@
 package pro.qyoga.tests.infra.web
 
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-import org.springframework.security.web.FilterChainProxy
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.servlet.client.MockMvcWebTestClient
-import org.springframework.web.context.WebApplicationContext
 import pro.qyoga.tests.infra.test_config.spring.baseUrl
 
 
 val mainWebTestClient: WebTestClient by lazy { createWebTestClient() }
 
-fun createWebTestClient(context: ConfigurableApplicationContext = pro.qyoga.tests.infra.test_config.spring.context): WebTestClient {
-    val mainSecurityFilterChain = context.getBean("mainSecurityFilterChain", SecurityFilterChain::class.java)
-    return MockMvcWebTestClient
-        .bindToApplicationContext(context as WebApplicationContext)
-        .apply(springSecurity(FilterChainProxy(mainSecurityFilterChain)))
-        .configureClient()
+fun createWebTestClient(context: ConfigurableApplicationContext = pro.qyoga.tests.infra.test_config.spring.context): WebTestClient =
+    WebTestClient.bindToServer()
         .baseUrl(context.baseUrl)
         .defaultHeader("Content-Type", "application/json;charset=UTF-8")
         .build()
-}
