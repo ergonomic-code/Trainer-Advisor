c03fe583152f0ff5f9197fc7d5ab313b8d480c3f
Aleksey Zhidkov <me@azhidkov.pro>
2026-03-01 11:57:57 +0700

    build/qg-xxx: обновлён импорт org.springframework.data.core.TypeInformation

--- app/src/main/kotlin/pro/qyoga/core/users/therapists/TherapistsRepo.kt
+++ app/src/main/kotlin/pro/qyoga/core/users/therapists/TherapistsRepo.kt
@@ -1,10 +1,10 @@
 package pro.qyoga.core.users.therapists
 
+import org.springframework.data.core.TypeInformation
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate
 import org.springframework.data.jdbc.core.convert.JdbcConverter
 import org.springframework.data.jdbc.repository.support.SimpleJdbcRepository
 import org.springframework.data.mapping.model.BasicPersistentEntity
-import org.springframework.data.util.TypeInformation
 import org.springframework.stereotype.Repository
 
 
