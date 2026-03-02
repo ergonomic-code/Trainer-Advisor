4c009232c0a75ca355c43d38196fd7cb9206e6e1
Aleksey Zhidkov <me@azhidkov.pro>
2026-03-02 18:57:00 +0700

    build/qg-xxx: исправлены ошибки компиляции связанные с распилом и переездом автогонфигураций в отдельные модули/пакеты

--- app/src/main/kotlin/pro/qyoga/i9ns/calendars/google/client/GoogleCalendarsClient.kt
+++ app/src/main/kotlin/pro/qyoga/i9ns/calendars/google/client/GoogleCalendarsClient.kt
@@ -11,7 +11,7 @@ import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.UserCredentials
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
+import org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientProperties
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
