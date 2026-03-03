c6c34afe2971c4a30221a7cf7709be10672667e0
Aleksey Zhidkov <me@azhidkov.pro>
2026-03-03 13:29:06 +0700

    build/qg-xxx: исправлена конфигурация AuthenticationProvider-а PasswordEncoder-ом в тестах
    Без этого SB ругался:
    
    WARN  o.s.s.c.a.a.c.InitializeUserDetailsBeanManagerConfigurer$InitializeUserDetailsManagerConfigurer - Global AuthenticationManager configured with an AuthenticationProvider bean. UserDetailsService beans will not be used by Spring Security for automatically configuring username/password login. Consider removing the AuthenticationProvider bean. Alternatively, consider using the UserDetailsService in a manually instantiated DaoAuthenticationProvider. If the current configuration is intentional, to turn off this warning, increase the logging level of 'org.springframework.security.config.annotation.authentication.configuration.InitializeUserDetailsBeanManagerConfigurer' to ERROR
    
    Не связано с миграцией на SB 4

--- app/src/testFixtures/kotlin/pro/qyoga/tests/infra/test_config/spring/auth/TestPasswordEncoderConfig.kt
+++ app/src/testFixtures/kotlin/pro/qyoga/tests/infra/test_config/spring/auth/TestPasswordEncoderConfig.kt
@@ -5,28 +5,17 @@ package pro.qyoga.tests.infra.test_config.spring.auth
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @TestConfiguration
-class TestPasswordEncoderConfig(
-    private val userDetailsService: UserDetailsService
-) {
+class TestPasswordEncoderConfig {
 
     // Стандартный BCryptPasswordEncoder кодирует пароли по 300мс, что слишком расточительно для тестов
     @Suppress("DEPRECATION")
     @Primary
     @Bean
     fun fastPasswordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
-
-    @Bean
-    fun daoAuthenticationProvider(): DaoAuthenticationProvider {
-        val daoAuthenticationProvider = DaoAuthenticationProvider(userDetailsService)
-        daoAuthenticationProvider.setPasswordEncoder(fastPasswordEncoder())
-        return daoAuthenticationProvider
-    }
 
 }
